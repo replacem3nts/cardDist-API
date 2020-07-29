bc5b40061d350a88e6b52a25e3243adc285aecdb
replacem3nts <donny.landis@gmail.com>
2020-07-29 11:53:59 -0400

    fixes rx serializer

--- app/serializers/rx_serializer.rb
+++ app/serializers/rx_serializer.rb
@@ -1,3 +1,3 @@
 class RxSerializer < ActiveModel::Serializer
-  attributes :id, :hcname, :cboname, :language, :prescribername, :prescriberphone, :clienttel, :amount, :appt, :cardserial, :pickedup, :loaded
+  attributes :id, :hcname, :cboname, :language, :prescribername, :prescriberphone, :prescriberemail, :clienttel, :amount, :appt, :cardserial, :pickedup, :loaded
 end
