41c9a0840d0fe37274639271f65a83d66dbd2d43
replacem3nts <donny.landis@gmail.com>
2020-08-11 17:02:07 -0400

    Adds hc auth methods

--- app/controllers/api/v1/hcs_controller.rb
+++ app/controllers/api/v1/hcs_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::HcsController < ApplicationController
+    before_action :authorized, only: [:persist_login]
 
     def login
+        @hc = Hc.find_by(username: params[:username])
+        if @hc && @hc.authenticate(params[:password]) 
+            wristband = encode_token({hc_id: @hc.id})
+            render json: {
+                hc: HcSerializer.new(@hc),
+                token: wristband
+            }
+        else
+            render json: {message: "Incorrect username or password"}
+        end
     end
 
     def persist_login
+        wristband = encode_token({hc_id: @hc.id})
+        render json: {
+            hc: HcSerializer.new(@hc),
+            token: wristband
+        }
     end
 end
