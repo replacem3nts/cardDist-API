5e677ce4384a2fef851911246a5e4f87484006e6
replacem3nts <donny.landis@gmail.com>
2020-08-17 15:56:24 -0400

    starts creating scaffolding for emailers

--- app/mailers/cbo_mailer.rb
+++ app/mailers/cbo_mailer.rb
@@ -1,2 +1,22 @@
 class CboMailer < ApplicationMailer
+    default from: 'confirmations@4ct-card.org'
+ 
+    def en_intake_email
+      @user = params[:user]
+      @url  = 'http://example.com/login'
+      mail(to: @user.email,
+           subject: 'Welcome to My Awesome Site',
+           template_path: 'notifications',
+           template_name: 'another')
+    end
+
+    def sp_intake_email
+      @user = params[:user]
+      @url  = 'http://example.com/login'
+      mail(to: @user.email,
+           subject: 'Welcome to My Awesome Site',
+           template_path: 'notifications',
+           template_name: 'another')
+    end
+  end
 end
